b400fe5c3dbf2edfb52ae7d2beec2eed403e1e71
luizandrioli <126569800+luizandrioli@users.noreply.github.com>
2024-03-07 20:07:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -181,9 +181,62 @@ function carregaListaDespesas(despesas=Array(),filtro=false){
 
     let ListaDespesas=document.getElementById("listadepesas")
     ListaDespesas.innerHTML=''
-    despesas.forEach(function(d)){
+    despesas.forEach(function(d){
+
 
         var linha = ListaDespesas.insertRow();
-    }
 
+        linha.insertCell(0).innerHTML=`${d.dia}/${d.mes}/${d.ano}`
+
+        switch(d.tipo){
+            case '1': d.tipo ='Alimentaçâo'
+                break
+            case '2': d.tipo ='Educação'
+                break 
+            case '3': d.tipo ='Lazer'
+                break
+            case '4': d.tipo ='Saúde'
+                break 
+            case '5': d.tipo ='Transporte'
+                break        
+        }
+        linha.insertCell(1).innerHTML=d.tipo
+        linha.insertCell(2).innerHTML=d.descricao
+        linha.insertCell(3).innerHTML=d.valor
+
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fa fa-times" ></i>'
+        btn.id = `id-despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesas_','')
+
+            bd.remover(id)
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+        console.log(4)
+    })
 }
+
+function pesquisarDespesas(){
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value
+    let descriçao = document.getElementById("descriçao").value
+    let valor = document.getElementById("valor").value
+
+    let despesas = new Despesa(ano,mes,dia,tipo,descriçao,valor)
+     
+    let despesas = bd.pesquisar(despesa)
+
+    this.carregarListaDespesa(despesas, true)
+}
+
+
+
+
+
+
+
